cbd7f08decfa6fe6bcac32afca5a312ddad47e11
Kyle Deal <kdeal@kyledeal.com>
2017-07-25 22:44:12 -0700

    Change functions around so this can be used as a library

--- devdocs_cli/__main__.py
+++ devdocs_cli/__main__.py
@@ -55,24 +55,8 @@ def make_request(endpoint, url, transform=False):
         raise error
 
 
-def get_docs(url):
-    return make_request('docs/docs.json', url)
-
-
 def get_index(doc_set, url):
     return make_request(path.join('docs', doc_set, 'index.json'), url)
-
-
-def get_db(doc_set, url):
-    return make_request(path.join('docs', doc_set, 'db.json'), url, transform=True)
-
-
-def view_in_elinks(string, tag='', browser='elinks'):
-    if tag:
-        tag = '#' + tag
-    with NamedTemporaryFile(suffix='.html') as tempfile:
-        tempfile.write(string.encode('utf-8'))
-        run((browser, tempfile.name + tag))
 
 
 def priority(query, string):
@@ -95,64 +79,76 @@ def search_dicts(docs, query, key):
     return matched_docs
 
 
-def docsets_handler(args):
-    docsets = get_docs(args.url)
-    for doc in search_dicts(docsets, ' '.join(args.filter), 'slug'):
-        print(doc['slug'])
+def docsets(docsets_filter, url):
+    all_docsets = make_request('docs/docs.json', url)
+    return search_dicts(all_docsets, ' '.join(docsets_filter), 'slug')
 
 
-def search_handler(args):
-    docset_index = get_index(args.doc_set, args.url)
+def search(doc_set, query, url):
+    docset_index = get_index(doc_set, url)
+    matched_docs = search_dicts(docset_index['entries'], ' '.join(query), 'name')
+    return matched_docs
+
+
+def view(doc_set, query, url):
+    docset_index = get_index(doc_set, url)
     matched_docs = search_dicts(
-        docset_index['entries'], ' '.join(args.query), 'name')
-    for doc in matched_docs:
-        print(doc['name'])
-
-
-def view_handler(args):
-    docset_index = get_index(args.doc_set, args.url)
-    matched_docs = search_dicts(
-        docset_index['entries'], ' '.join(args.query), 'name')
+        docset_index['entries'], ' '.join(query), 'name')
     if matched_docs:
         doc_path = matched_docs[0]['path']
-        html_db = get_db(args.doc_set, args.url)
+        html_db = make_request(path.join('docs', doc_set, 'db.json'), url, transform=True)
         tag = ''
         if '#' in doc_path:
             doc_path, tag = doc_path.split('#', maxsplit=1)
-        view_in_elinks(html_db[doc_path], tag, args.browser)
+        return html_db[doc_path], tag
+
+
+def view_handler(browser, **view_args):
+    html, tag = view(**view_args)
+    if tag:
+        tag = '#' + tag
+    with NamedTemporaryFile(suffix='.html') as tempfile:
+        tempfile.write(html.encode('utf-8'))
+        run((browser, tempfile.name + tag))
+
+
+def print_handler(key, func, **kwargs):
+    results = func(**kwargs)
+    for item in results:
+        print(item[key])
 
 
 def create_parser():
     parser = ArgumentParser(description='Query devdocs.io')
-    parser.add_argument(
-        '-u', '--url', help='base url of devdocs', default=DEFAULT_URL)
+    parser.add_argument('-u', '--url', help='base url of devdocs', default=DEFAULT_URL)
     subparsers = parser.add_subparsers()
 
-    search = subparsers.add_parser('search', help='search through one doc set')
-    search.add_argument('doc_set', help='Document set to search')
-    search.add_argument('query', nargs='*', help='Query to search')
-    search.set_defaults(func=search_handler)
+    search_parser = subparsers.add_parser('search', help='search through one doc set')
+    search_parser.add_argument('doc_set', help='Document set to search')
+    search_parser.add_argument('query', nargs='*', help='Query to search')
+    search_parser.set_defaults(handler=print_handler, func=search, key='name')
 
-    view = subparsers.add_parser('view', help='view through one doc set')
-    view.add_argument(
+    view_parser = subparsers.add_parser('view', help='view through one doc set')
+    view_parser.add_argument(
         '-b', '--browser', help='Browser to view docs in',
         default=os.environ.get('BROWSER', 'elinks'),
     )
-    view.add_argument('doc_set', help='Document set to view')
-    view.add_argument('query', nargs='*', help='Query to view')
-    view.set_defaults(func=view_handler)
+    view_parser.add_argument('doc_set', help='Document set to view')
+    view_parser.add_argument('query', nargs='*', help='Query to view')
+    view_parser.set_defaults(handler=view_handler)
 
-    docsets = subparsers.add_parser(
-        'docsets', help='search through one doc set')
-    docsets.add_argument('filter', nargs='*', help='filter docsets')
-    docsets.set_defaults(func=docsets_handler)
+    docsets_parser = subparsers.add_parser('docsets', help='search through one doc set')
+    docsets_parser.add_argument('docsets_filter', metavar='filter', nargs='*', help='filter docsets')
+    docsets_parser.set_defaults(handler=print_handler, func=docsets, key='slug')
 
     return parser
 
 
 def main():
     args = create_parser().parse_args()
-    args.func(args)
+    handler_args = vars(args)
+    handler = handler_args.pop('handler')
+    handler(**handler_args)
 
 
 if __name__ == '__main__':
